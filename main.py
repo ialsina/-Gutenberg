4f3dd6ec5e40827032981cde7e5e1c318232711b
Ivan Alsina <ivanalsina@protonmail.com>
2022-09-06 17:10:34 +0200

    Full Parsing GUTINDEX.ALL (need to address some errors and improve style)

--- main.py
+++ main.py
@@ -1,40 +1,123 @@
 import re
 from itertools import zip_longest
 from utils import readfile, writefile, Book, Library
-
+import sys
 
 
 def parse_gutindex(gutindex_raw):
     pattern = r'(<===LISTINGS===>\n)(.*)(<==End of GUTINDEX.ALL==>)'
-    gutindex_stripped = re.search(pattern, gutindex_raw, flags=re.S).groups()[1]
-    findings = list(re.finditer('={2,}', gutindex_stripped))
-    years = []
+    main_match = re.search(pattern, gutindex_raw, flags=re.S)
+    main_span = main_match.span()
+    gutindex_stripped = main_match.groups()[1]
+    years_text = separate_by_year(gutindex_stripped)
+    years_book = {year: parse_year(text, year) for year, text in years_text.items()}
+    library = Library('Gutenberg')
+    for _, books in years_book.items():
+        for book in books:
+            library.add(book)
+    return library
 
-    for cur, nex in zip_longest(findings, findings[1:], fillvalue=None):
-        val_beg = cur.span()[1]
-        if nex is None:
-            val_end = len(gutindex_stripped)
+
+
+
+def separate_by_year(gutindex_stripped):
+    headers = list(re.finditer(r'[\s\t]+GUTINDEX\.(\d{4})[\s\t]*\n', gutindex_stripped))
+    years_dict = dict()
+    for cur_header, nex_header in zip_longest(headers, headers[1:]):
+        year = int(cur_header.groups()[0])
+        pos0 = cur_header.span()[1]
+        if nex_header:
+            pos1 = nex_header.span()[0]
         else:
-            val_end = nex.span()[0]
-        years.append(gutindex_stripped[val_beg:val_end])
+            pos1 = len(gutindex_stripped)
+        cur_text = gutindex_stripped[pos0:pos1] 
+        years_dict[year] = cur_text
+    return years_dict
 
-    return years
+
+def parse_year(text_raw, year=None):
+    pattern = r'(TITLE.*?\n)(.*)'
+    matches = re.search(pattern, text_raw, flags=re.S)
+    pattern_sub = r'(~ ~ ~ ~)(.*)(~ ~ ~ ~)'
+    text_data = matches.groups()[1]
+    text_data = re.sub(pattern_sub, '', text_data)
+    text_data = re.sub(pattern, '', text_data)
+    books_text = [el.strip() for el in text_data.split('\n\n') if el != '']
+    books = [parse_book(el, year) for el in books_text]
+    books = [el for el in books if isinstance(el, Book)]
+    return books
 
 
 def find_occurrences(target, text):
     pass
 
 
-def parse_book(gugtenberg_raw):
-    pattern = r'(\*\*\*.*START.*\*\*\*)(.*)(\*\*\*.*END.*\*\*\*)'
-    search = re.search(pattern, text, flags=re.S)
-    book = search.groups()[1]
-    return book
+
+def parse_book(text_raw, year=None):
+    text_lines = []
+    text_raw_lines = text_raw.split('\n')
+    pattern = r'(\D+\s+)(\d+\w?)'
+    search = re.search(pattern, text_raw_lines[0])
+    if search is None:
+        print("Error in:", text_raw)
+        return "Error"
+    text_lines.append(search.groups()[0].strip())
+    gutid = search.groups()[1]  
+    text_lines.extend(text_raw_lines[1:])
+    column1 = ' '.join(text_lines)
+    attrs = parse_elements(column1)
+    return Book(gutid=gutid, gutyear=year, **attrs)
+
+
+# WORK IN PROGRESS. PROBLEM TRYING TO SOLVE: TITLE OVER MULTIPLE LINES (ex. 40233)
+def parse_book(text_raw, year=None):
+    text_lines = text_raw.split('\n')
+    pattern = r'\s?(.+\S+)\s+(\d+\w?)'
+    for i, line in enumerate(text_lines):
+        search = re.search(pattern, line)
+        if search:
+            break
+        else:
+            print(i, line)
+    else:
+        print("Error in:", text_raw)
+        return "Error"
+    text_lines[i] = search.groups()[0]
+    gutid = search.groups()[1]
+    column1 = ' '.join(text_lines)
+    attrs = parse_elements(column1)
+    return Book(gutid=gutid, gutyear=year, **attrs)
+
+
+def parse_elements(line):
+
+    attrs = {}
+    pattern = r'\[(.*?):(.*?)\]'
+    title_author = re.sub(pattern, '', line).strip()
+    sep = ', by '
+    sep_count = title_author.count(sep)
+    if sep_count == 1:
+        title, author = title_author.split(sep)
+    elif sep_count == 0:
+        title = title_author
+        author = ''
+    elif sep_count > 1:
+        separated = title_author.split(sep)
+        title = sep.join(separated[:-1])
+        author = separated[-1]
+    attrs['title'] = title
+    attrs['author'] = author
+    for match in re.finditer(pattern, line):
+        attrs.update({match.groups()[0].strip(): match.groups()[1].strip()})
+    return attrs
+    
+
 
 
 text_example = readfile('data/example.txt')
 
-years = parse_gutindex(readfile('data/GUTINDEX.ALL'))
+library = parse_gutindex(readfile('data/GUTINDEX.ALL'))
 
-for i, text in enumerate(years):
-    writefile('data/years/year_{:0>2d}.txt'.format(i), text)
+#for year, text in years.items():
+#    writefile('data/years/year_{:0>2d}.txt'.format(year), text)
+
--- utils.py
+++ utils.py
@@ -1,30 +1,128 @@
+import random
+from copy import deepcopy
+import requests
+
 def readfile(path):
-	with open(path, 'r') as f:
-		return f.read()
+    with open(path, 'r') as f:
+        return f.read()
 
 def writefile(path, content):
-	with open(path, 'w') as f:
-		f.write(content)
+    with open(path, 'w') as f:
+        f.write(content)
 
 
 class Library:
-	def __init__(self, name):
-		self.name = name
-		self._library = set()
+    def __init__(self, name):
+        self.name = name
+        self._library = set()
 
-	def add(self, book):
-		assert isinstance(book, Book)
-		self._library.add(book)
+    def __len__(self):
+        return len(self._library)
 
-	def browse(self, language=None, **kwargs):
-		kwargs.update(language=language)
-		findings = set()
-		for book in self._library:
-			if book.check(**kwargs):
-				findings.add(book)
-		return findings
+    def __iter__(self):
+        return iter(self._library)
+
+
+    def add(self, book):
+        assert isinstance(book, Book)
+        self._library.add(book)
+
+    def browse(self, language=None, **kwargs):
+        kwargs.update(language=language)
+        findings = set()
+        for book in self._library:
+            if book.check(**kwargs):
+                findings.add(book)
+        return findings
+
+    def browse_one(self, language=None, **kwargs):
+        kwargs.update(language=language)
+        library = deepcopy(self._library)
+        while library:
+            book = library.pop()
+            if book.check(**kwargs):
+                return book
+        return None
+
+    def pick(self, n=1):
+        result = random.sample(self._library, k=n)
+        if n == 1:
+            return result[0]
+        else:
+            return result
+    
+
 
 
 class Book:
-	def __init__(self, text_to_parse):
-		raise NotImplementedError()
+
+    CHECK_DEFAULTS_EQUAL = {'author': False, 'subtitle': False}
+
+    def __init__(self, title = '', author = '', gutid = None, gutyear = None, **kwargs):
+        self.title = title
+        self.author = author
+        self.gutid = gutid
+        self.gutyear = gutyear
+        for key, val in kwargs.items():
+            setattr(self, key.lower().strip(), val.strip())
+        if not hasattr(self, 'language'):
+            self.language = 'english'
+        else:
+            self.language = self.language.lower()
+
+    def __repr__(self):
+        return self.title + ', by ' + self.author
+
+    @property
+    def attrs(self):
+        return self.__dict__
+
+    @property
+    def url(self):
+        return "https://www.gutenberg.org/files/{0}/{0}-0.txt".format(self.gutid)
+
+
+
+    def get_html(self):
+        session = requests.Session()
+        session.mount("http://", requests.adapters.HTTPAdapter(max_retries=2))
+        session.mount("https://", requests.adapters.HTTPAdapter(max_retries=2))
+        response = session.get(self.url)
+        if response.status_code == 200:
+            return response.text
+        else:
+            raise Exception
+        
+
+    
+
+    def check(self, default=True, equal=None, **kwargs):
+        for key, val in kwargs.items():
+            if val is None:
+                continue
+            if not self._condition(key, val, default=default, equal=equal):
+                return False
+        return True
+
+    def _condition(self, key, val, default=True, equal=None):
+        """Checks if the instance's attribute 'key' has the value 'val'
+        key: attribute to check
+        val: value towards which the attribute is checked
+        default: if False: the checking is done via the value of 'equal'
+                 if True: the checking is done via the default behavior
+        equal: if True: the checking is done via an equality
+               if False: the checking is done via a 'contains'
+        Note: if default is True, equal is ignored
+        Note: if the instance doesn't have the attribute 'key', returns
+              False
+        """
+        if not hasattr(self, key):
+            return False
+        if default:
+            _equal = self.CHECK_DEFAULTS_EQUAL.get(key, True)
+        else:
+            _equal = equal
+        if _equal:
+            return getattr(self, key) == val
+        else:
+            return val in getattr(self, key)
